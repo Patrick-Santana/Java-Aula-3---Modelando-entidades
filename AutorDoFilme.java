e03154016a9ad37df63f8fb2202dd82ec721043c
Patrick-Santana <zonaabissal2016@gmail.com>
2019-08-21 10:23:47 -0300

    Create AutorDoFilme.java

---
+++ AutorDoFilme.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+ public class Autor{
+
+ 	  String nome; // nome do filme
+    Date dataDeLancamento; // Data de Lançamento
+    String genero; // tipo do filme (terror, comédia)
+    Autor autor;
+
+
+}
